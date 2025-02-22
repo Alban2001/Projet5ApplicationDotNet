f5b315622d491c9941f129d930ed497a88d48d27
Alban Voiriot <alban.voiriot@gmail.com>
2025-02-22 18:12:09 +0100

    Base project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet5ApplicationDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
